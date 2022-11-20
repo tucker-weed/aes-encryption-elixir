0ccf0cd210e87483401dc200b4c8ab7acb410758
Tucker Weed <tuckerjweed@gmail.com>
2022-11-20 13:34:13 -0500

    [refactor] use setup for testing

--- test/aes_test.exs
+++ test/aes_test.exs
@@ -2,10 +2,14 @@ defmodule AESTest do
   use ExUnit.Case
   doctest AES
 
-  test "ciphertext will decrypt to the original plaintext" do
+  setup do
+    original_plaintext = "This is a random message to encrypt"
+    %{opt1: original_plaintext}
+  end
+
+  test "ciphertext will decrypt to the original plaintext", %{opt1: original_plaintext} do
     salt = AES.generate_salt()
     secret = AES.generate_secret("fakePassword", salt)
-    original_plaintext = "This is a random message to encrypt"
     ciphertext = AES.encrypt(original_plaintext, secret)
     secret = AES.generate_secret("fakePassword", salt)
     plaintext = AES.decrypt(ciphertext, secret)
